d65738e0890bd9dde3f0a37f69fee0f3f0f1962b
cegekaJG <cegekaJG@users.noreply.github.com>
2024-05-03 13:04:49 +0000

    Deploying AL-Go from publish-testmaster (ceb45e783df2ffb684c9a9938328b2c919b43f17) to publish-testmaster

--- .github/DeployToOnPrem.ps1
+++ .github/DeployToOnPrem.ps1
@@ -13,42 +13,59 @@ Param(
     }
 )
 
-$ErrorActionPreference = "Stop"
-$parameters | ConvertTo-Json -Depth 99 | Out-Host
+function New-TemporaryFolder {
+    $tempPath = Join-Path ([System.IO.Path]::GetTempPath()) ([GUID]::NewGuid().ToString())
+    New-Item -ItemType Directory -Path $tempPath | Out-Null
 
-$tempPath = Join-Path ([System.IO.Path]::GetTempPath()) ([GUID]::NewGuid().ToString())
-New-Item -ItemType Directory -Path $tempPath | Out-Null
-
-Copy-AppFilesToFolder -appFiles $parameters.apps -folder $tempPath | Out-Null
-$appsList = @(Get-ChildItem -Path $tempPath -Filter *.app)
-if (-not $appsList -or $appsList.Count -eq 0) {
-    Write-Host "::error::No apps to publish found."
-    exit 1
+    return $tempPath
 }
 
-if ($appsList.Count -gt 1) {
-    $appsList = Sort-AppFilesByDependencies -appFiles $appsList
-    $appsList = $appsList | ForEach-Object { [System.IO.FileInfo]$_ }
-    Write-Host "Publishing a total of $($appsList.Count) app(s):"
-    $appsList | ForEach-Object { Write-Host "- $($_.Name)" }
+function Get-AppList {
+    param (
+        [string[]]$appPaths,
+        [string]$outputPath
+    )
+    Copy-AppFilesToFolder -appFiles $appPaths -folder $outputPath | Out-Null
+    $appsList = @(Get-ChildItem -Path $outputPath -Filter *.app)
+    if (-not $appsList -or $appsList.Count -eq 0) {
+        Write-Host "::error::No apps to publish found."
+        exit 1
+    }
+
+    if ($appsList.Count -gt 1) {
+        $appsList = Sort-AppFilesByDependencies -appFiles $appsList
+        $appsList = $appsList | ForEach-Object { [System.IO.FileInfo]$_ }
+        Write-Host "Publishing a total of $($appsList.Count) app(s):"
+        $appsList | ForEach-Object { Write-Host "- $($_.Name)" }
+    }
+    else {
+        Write-Host "Publishing $($appsList[0].Name)"
+    }
+
+    return $appsList
 }
-else {
-    Write-Host "Publishing $($appsList[0].Name)"
+
+function Get-PublishScript {
+    param (
+        [string]$url = "https://raw.githubusercontent.com/CBS-BC-AT-Internal/INT.utilities/v0.2.0/powershell/Install-NAVApp.ps1",
+        [string]$outputPath
+    )
+    Write-Host "`nDownloading the deployment script"
+    Write-Host "URL: $url"
+    $deployScriptPath = Join-Path -Path $outputPath -ChildPath "Deploy-ToBC.ps1"
+    Invoke-WebRequest -Uri $url -OutFile $deployScriptPath
+    Write-Host "Downloaded the deployment script to $deployScriptPath"
+
+    return $deployScriptPath
 }
 
-# Download the deployment script
-Write-Host "`nDownloading the deployment script"
-
-$url = "https://raw.githubusercontent.com/CBS-BC-AT-Internal/INT.utilities/v0.2.0/powershell/Install-NAVApp.ps1"
-$deployScriptPath = Join-Path -Path $tempPath -ChildPath "Deploy-ToBC.ps1"
-
-Write-Host "URL: $url"
-Invoke-WebRequest -Uri $url -OutFile $deployScriptPath
-Write-Host "Downloaded the deployment script to $deployScriptPath"
-
-foreach ($app in $appsList) {
+function Deploy-App {
+    param (
+        [string]$srvInst,
+        [System.IO.FileInfo]$app
+    )
     Write-Host "`nDeploying app '$($app.Name)'"
-    $srvInst = $parameters.EnvironmentName
+    $appPath = $app.FullName
 
     # Deploy the app using the downloaded script
     try {
@@ -60,8 +77,23 @@ foreach ($app in $appsList) {
     }
 }
 
-Write-Host "`nSuccessfully deployed all apps to $($parameters.EnvironmentName)"
+function Remove-TempFiles {
+    param (
+        [string]$tempPath
+    )
+    Remove-Item -Path $outputPath -Recurse -Force | Out-Null
+    Write-Host "Removed temporary files."
+}
 
-# Cleanup
-Remove-Item -Path $tempPath -Recurse -Force | Out-Null
-Write-Host "Removed temporary files."
+$ErrorActionPreference = "Stop"
+$parameters | ConvertTo-Json -Depth 99 | Out-Host
+$tempPath = New-TemporaryFolder
+$appsList = Get-AppList -appFiles $parameters.apps -outputPath $tempPath
+$deployScriptPath = Get-PublishScript -outputPath $tempPath
+
+foreach ($app in $appsList) {
+    Deploy-App -srvInst $parameters.EnvironmentName -app $app
+}
+
+Write-Host "`nSuccessfully deployed all apps to $($parameters.EnvironmentName)"
+Remove-TempFiles -tempPath $tempPath
